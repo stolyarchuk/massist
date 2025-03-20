94dfc5025d86983b5f0c25ff86b1c6af11c8e2cd
Roman Stolyarchuk <roman@stolyarch.uk>
2025-03-20 21:31:40 +0400

    feat: add main application entry point with logging and Playground integration

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-# Standard library imports
 import logging
-import sys
-from textwrap import dedent
 
 from agno.playground.playground import Playground
 from agno.playground.serve import serve_playground_app
-from agno.tools.duckduckgo import DuckDuckGoTools
-# Third-party imports
 from dotenv import load_dotenv
 
-from config import config
 from llmx.agents import mitigator_assistant
-from logger import init_module_loggers, logger, update_formatters
-
-# from agno.reranker.base import Reranker  # Unused import
-# from agno.reranker.cohere import CohereReranker  # Unused import
-
 
 load_dotenv()
 # loggers = [logging.getLogger().name]
@@ -37,9 +26,6 @@ init_module_loggers(*loggers)
 app = Playground(agents=[mitigator_assistant]).get_app()
 
 if __name__ == "__main__":
-
-    # knowledge_base.load(recreate=False, upsert=False)
     # knowledge_base.load(recreate=True, upsert=True)
-    # kb.load(recreate=True, upsert=True)
 
-    serve_playground_app("massist:app", reload=True)
+    serve_playground_app("main:app", reload=True)
