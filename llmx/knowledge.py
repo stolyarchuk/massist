5de15b2990b38a15a066b0df59551730eca60ea8
Roman Stolyarchuk <roman@stolyarch.uk>
2025-03-22 04:16:40 +0400

    feat: refactor storage and knowledge management
    - Replaced PostgreSQL storage with SQLite storage for the Mitigator Assistant.
    - Updated vector database integration to use LanceDB with dynamic table naming.
    - Refactored chunking strategy to utilize a function for agent chunking.
    - Removed unnecessary print statement in vector database build process.

--- llmx/knowledge.py
+++ llmx/knowledge.py
@@ -1,15 +1,14 @@
 from agno.knowledge.website import WebsiteKnowledgeBase
 
 from config import config
-from llmx.chunking import agent_chunking
-from llmx.vectordb import pgvector
+from llmx.chunking import get_agent_chanking
+from llmx.vectordb import get_vectordb
 
 knowledge_base = WebsiteKnowledgeBase(
     urls=[config.WEBSITE_URL],
     max_links=config.MAX_LINKS,
     max_depth=6,
-    vector_db=pgvector,
-    # optimize_on=5000,
+    vector_db=get_vectordb(1024),
     num_documents=3,
-    chunking_strategy=agent_chunking
+    chunking_strategy=get_agent_chanking(1024)
 )
--- llmx/vectordb.py
+++ llmx/vectordb.py
@@ -1,3 +1,4 @@
+from agno.vectordb.lancedb.lance_db import LanceDb
 from agno.vectordb.pgvector.pgvector import PgVector
 from agno.vectordb.search import SearchType
 
@@ -6,9 +7,18 @@ from llmx.models import vllm_embedder
 
 pgvector = PgVector(
     db_url=config.DB_URL,
-    table_name="massist_embeddings_768",
+    table_name="massist_embeddings_1024",
     # schema="ai",
     embedder=vllm_embedder,
-    search_type=SearchType.hybrid,
+    search_type=SearchType.vector,
     content_language="russian",
 )
+
+
+def get_vectordb(size: int):
+    return LanceDb(
+        table_name=f"massist_embeddings_{size}",
+        uri="tmp/lancedb",
+        search_type=SearchType.vector,
+        embedder=vllm_embedder
+    )
