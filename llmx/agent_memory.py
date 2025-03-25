0a65b7be344bec0bf2d7f77a4d3123a4bf1fecdd
Roman Stolyarchuk <roman@stolyarch.uk>
2025-03-25 15:50:57 +0400

    feat: implement memory and storage management for agents
    - Added `agent_memory.py` to handle agent memory retrieval.
    - Introduced `memory_db.py` for dynamic memory database selection.
    - Created `storage_db.py` for dynamic storage database selection.
    - Updated `agents.py` to integrate memory management into agent creation.
    - Refactored `knowledge.py` to use the new vector database module.
    - Adjusted `main.py` to reflect changes in agent initialization.

---
+++ llmx/agent_memory.py
@@ -0,0 +1,32 @@
+from agno.memory.agent import AgentMemory
+from agno.memory.classifier import MemoryClassifier
+from agno.memory.manager import MemoryManager
+from agno.memory.summarizer import MemorySummarizer
+from agno.memory.team import TeamMemory
+from agno.models.base import Model
+
+from llmx.memory_db import get_memory_db
+from llmx.models import or_gemini2_flash
+
+
+def get_memory(agent_id: str, model: Model = or_gemini2_flash) -> AgentMemory:
+    return AgentMemory(
+        user_id="stolyarchuk",
+        db=get_memory_db(agent_id),
+        create_user_memories=True,
+        create_session_summary=True,
+        manager=MemoryManager(model=model),
+        classifier=MemoryClassifier(model=model),
+        summarizer=MemorySummarizer(model=model),
+    )
+
+
+def get_team_memory(agent_id: str, model: Model = or_gemini2_flash) -> TeamMemory:
+    return TeamMemory(
+        user_id="stolyarchuk",
+        db=get_memory_db(agent_id),
+        create_user_memories=True,
+        # create_session_summary=True,
+        manager=MemoryManager(model=model),
+        classifier=MemoryClassifier(model=model),
+    )
--- llmx/memory_db.py
+++ llmx/memory_db.py
@@ -1,14 +1,10 @@
-from agno.memory.agent import AgentMemory
-from agno.memory.classifier import MemoryClassifier
 from agno.memory.db.base import MemoryDb
 from agno.memory.db.mongodb import MongoMemoryDb
 from agno.memory.db.postgres import PgMemoryDb
 from agno.memory.db.sqlite import SqliteMemoryDb
-from agno.memory.manager import MemoryManager
-from agno.memory.summarizer import MemorySummarizer
 
 from config import config
-from llmx.models import gemini_model, hf_model, or_gemini2_flash, vllm_model
+
 
 
 def get_memory_db(agent_id: str) -> MemoryDb:
@@ -32,14 +28,3 @@ def get_memory_db(agent_id: str) -> MemoryDb:
         )
 
     raise ValueError("bad storage class name")
-
-
-# memory = AgentMemory(
-#     user_id="stolyarchuk",
-#     db=mongo_memory,
-#     create_user_memories=True,
-#     create_session_summary=True,
-#     manager=MemoryManager(model=vllm_model),
-#     classifier=MemoryClassifier(model=google_model),
-#     summarizer=MemorySummarizer(model=google_model),
-# )
