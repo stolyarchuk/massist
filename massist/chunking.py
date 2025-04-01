e42a4f8e2dd3444b2f2b1a5c3a563c2d69b902fd
Roman Stolyarchuk <roman@stolyarch.uk>
2025-04-01 22:34:39 +0400

    feat: enhance chunking strategy and configuration settings
    - Updated API endpoint from `/openapi.json` to `/docs`
    - Added MAX_DEPTH and CHUNKING_STRATEGY to configuration
    - Modified get_chunking_strategy to support multiple strategies: agentic, recursive, and document
    - Updated get_kb function to accept max_links and max_depth parameters
    - Refactored vectorize.py to utilize the new deepseek model

--- massist/chunking.py
+++ massist/chunking.py
@@ -1,9 +1,20 @@
 from agno.document.chunking.agentic import AgenticChunking
+from agno.document.chunking.document import DocumentChunking
+from agno.document.chunking.recursive import RecursiveChunking
 from agno.document.chunking.strategy import ChunkingStrategy
 from agno.models.base import Model
 
 from config import config
 
 
-def get_chunking_strategy(model: Model) -> ChunkingStrategy:
-    return AgenticChunking(model=model, max_chunk_size=config.MAX_CHUNK_SIZE)
+def get_chunking_strategy(model: Model, max_chunk_size: int = config.MAX_CHUNK_SIZE) -> ChunkingStrategy:
+    if config.CHUNKING_STRATEGY == "agentic":
+        return AgenticChunking(model=model, max_chunk_size=max_chunk_size)
+
+    if config.CHUNKING_STRATEGY == "recursive":
+        return RecursiveChunking(chunk_size=max_chunk_size)
+
+    if config.CHUNKING_STRATEGY == "document":
+        return DocumentChunking(chunk_size=max_chunk_size)
+
+    raise ValueError("bad chunking strategy")
--- vectorize.py
+++ vectorize.py
@@ -1,19 +1,29 @@
 from massist.knowledge import get_kb
 from massist.logger import init_logging
-from massist.models import (get_gemini_pri_model, get_mistral_model,
-                            get_openrouter_model)
+from massist.models import (get_deepseek_model, get_gemini_pri_model,
+                            get_mistral_model, get_openrouter_model)
 
 init_logging()
 
 if __name__ == "__main__":
-    chunking_model = get_mistral_model(temperature=0.6)
+    chunking_model = get_mistral_model()
 
-    get_kb(topic="install", model=get_gemini_pri_model()).load(
-        recreate=True, upsert=True
-    )
-    # get_kb(topic="integrate").load(recreate=True, upsert=True)
-    # get_kb(topic="versions").load(recreate=True, upsert=True)
-    # get_kb(topic="maintenance").load(recreate=True, upsert=True)
+    # get_kb(topic="index", model=chunking_model, max_links=20, max_depth=1).load(
+    #     recreate=True, upsert=True
+    # )
+
+    # get_kb(topic="install", model=chunking_model,).load(
+    #     recreate=True, upsert=True
+    # )
+
+    # get_kb(topic="integrate", model=chunking_model).load(
+    #     recreate=True, upsert=True
+    # )
+    # get_kb(topic="versions", model=chunking_model).load(
+    #     recreate=True, upsert=True
+    # )
+    get_kb(topic="maintenance", model=get_deepseek_model(temperature=0.7), max_links=20).load(
+        recreate=True, upsert=True)
     # get_kb(topic="kb").load(recreate=True, upsert=True)
     # get_kb(topic="psg").load(recreate=True, upsert=True)
     # get_kb(topic="contact").load(recreate=True, upsert=True)
