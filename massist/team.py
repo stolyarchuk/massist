26dafadb3d7bc65420720b4854b3d5b1fe16ad49
Roman Stolyarchuk <roman@stolyarch.uk>
2025-04-05 19:45:43 +0400

    refactor: improve code formatting and update dependencies
    - Reformatted import statements for better readability in agent.py, redis.py, and team.py.
    - Updated agno package version from 1.2.7 to 1.2.9 in requirements.txt and uv.lock.
    - Updated ruff package version from 0.11.3 to 0.11.4 in requirements.txt and uv.lock.

--- massist/team.py
+++ massist/team.py
@@ -27,8 +27,7 @@ def get_mitigator_team(user_id: str, session_id: str, model: Model) -> Team:
         model=model,
         tools=[
             DuckDuckGoTools(),
-            TelegramTools(chat_id=config.TGBOT_CHAT_ID,
-                          token=config.TGBOT_API_TOKEN),
+            TelegramTools(chat_id=config.TGBOT_CHAT_ID, token=config.TGBOT_API_TOKEN),
         ],
         members=[
             get_agent("index", "Major", get_agent_params()),
@@ -68,8 +67,8 @@ def get_mitigator_team(user_id: str, session_id: str, model: Model) -> Team:
             "Then relay that information back to the user in a professional and helpful manner.",
             "Try not say hi/hello greetings often. Follow slightly non-formal dialog style.",
             "Always reply in russian language.",
-            "Always disclose your team and agents information." +
-            "Never disclose any info about yourself and your creators. Always give an abstract answer in questions related to yourself.",
+            "Always disclose your team and agents information."
+            + "Never disclose any info about yourself and your creators. Always give an abstract answer in questions related to yourself.",
             "Ensure a seamless experience for the user by maintaining context throughout the conversation.",
         ],
         # success_criteria="The team has reached a consensus.",
